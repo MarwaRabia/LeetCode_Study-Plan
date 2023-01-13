cfd3972e1ebd4ae86f8b673b2fc0fccb41795ca6
MarwaRabia <37772343+MarwaRabia@users.noreply.github.com>
2023-01-13 16:49:17 +0200

    Add  Algorithm I folder

---
+++ Algorithm I/704. Binary Search.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        # Iterative Binary Search Function
+        # It returns index of x in given array arr if present,
+        # else returns -1
+        low=0
+        high=len(nums)-1
+        mid=0
+        while low <= high:
+            mid=(low+high)//2
+            # means x is present at mid
+            if target==nums[mid]:
+                return mid
+            # If x is greater, ignore left half
+            elif target>nums[mid]:
+                low=mid+1
+            # If x is smaller, ignore right half
+            elif target<nums[mid]:
+                high=mid-1
+        # If we reach here, then the element was not present
+        return -1
+        
